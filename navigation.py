c3cfe705b25979a96ebffa43ba4dbfa5e916797b
Akki283 <akshaygaikwad283@gmail.com>
2022-12-26 13:32:12 +0530

    2nd commit

--- navigation.py
+++ navigation.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 driver=webdriver.Chrome(executable_path="D:\\dammy\\chromedriver.exe")
 
-driver.get("https://rahulshettyacademy.com/")
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 print(driver.title)
 print(driver.current_url)
+driver.find_element_by_id("name").send_keys("manish")
+driver.find_element_by_xpath("//input[@id='alertbtn']").click()
